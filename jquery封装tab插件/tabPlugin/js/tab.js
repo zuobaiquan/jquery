c9a9dfa70abd9f0de953519675b0109ac9fbe302
左佰全 <zuobaiquan@zuobaiquandeMacBook-Pro.local>
2017-10-07 20:43:50 +0800

    update code

--- jquery封装tab插件/tabPlugin/js/tab.js
+++ jquery封装tab插件/tabPlugin/js/tab.js
@@ -1,102 +1,112 @@
 (function ($) {
     var Tab = function (tab) {
-        var that = this;
-        //保存单个tab组件
-        this.tab = tab;
-        //配置默认参数
+        var _that=this;
+        this.tab=tab;
         this.config = {
             'triggerType': 'mouseover',  //鼠标触发类型
             'effect': 'default',        //内容切换效果
             'invoke': 1,             //默认显示第几个tab
             'auto': false             //定义是否自动切换
         };
-        //如果配置参数存在，就扩展默认的配置参数
-        if (this.getConfig()) {
-            $.extend(this.config, this.getConfig());
-        }
+        console.log(this.config);
+        if(this.getConfig()){
+            $.extend(this.config,this.getConfig());
+        };
+
+
         //保存tab标签列表,对应的内容列表
         this.tabItems = this.tab.find("ul.tab-nav li");
+
         this.contentItems = this.tab.find("div.content-wrap div.content-item");
-        //保存配置参数
+
         var config = this.config;
-        if (config.triggerType === 'click') {
-            this.tabItems.bind(config.triggerType, function () {
-                that.invoke($(this));
+
+        if(config.triggerType=='click'){
+            this.tabItems.bind(config.triggerType,function(){
+                _that.invokeStyle($(this));
             })
-        } else if (config.triggerType === 'mouseover') {
-            this.tabItems.bind(config.triggerType, function () {
-                alert(2)
-            });
-        } else {
+        }
+        else if(config.triggerType=='mouseover'){
+            this.tabItems.bind(config.triggerType,function(){
+                _that.invokeStyle($(this));
+            })
+        }
+        else{
             this.tabItems.mouseover(function () {
-                alert(2)
+                _that.invokeStyle($(this));
             });
         }
-        //自动切换
-        if (config.auto) {
-            //定义一个全局的定时器
-            this.timer = null;
-            //计数器
-            this.loop = 0;
+
+        if(config.auto){
+            this.timer=null;
+            this.loop=0;
             this.autoPlay();
+
+            //鼠标移入，停止tab切换，移除自动播放
             this.tab.hover(function () {
-                window.clearInterval(that.timer);
+                clearInterval(_that.timer);
             }, function () {
-                that.autoPlay();
+                _that.autoPlay();
             });
         }
+
         //设置默认显示第几个tab
         if (config.invoke > 1) {
-            this.invoke(this.tabItems.eq(config.invoke - 1));
+            this.invokeStyle(this.tabItems.eq(config.invoke - 1));
         }
     };
 
     Tab.prototype = {
-        //获取配置参数
-        getConfig: function () {
-            //获取tab节点上的data-config
-            var config = this.tab.attr("data-config");
-            //确保有配置参数
-            if (config && config != "") {
+        getConfig:function(){
+            var config=this.tab.attr('data-config');
+            if(config&&config!==''){
+                //参数已经配置
+                //console.log($.parseJSON(config));
                 return $.parseJSON(config);
-            } else {
+            }else{
+                //人工参数未配置
                 return null;
             }
         },
-        //事件驱动函数
-        invoke: function (currentTab) {
-            var that = this;
-            var index = currentTab.index();
-            currentTab.addClass('active').siblings().removeClass('active');
+        //时间驱动函数，改变内容和样式
+        invokeStyle:function(currentTab){
+            //获取切换效果
             var effect = this.config.effect;
-            var contentItems = this.contentItems;
-            if (effect === 'default') {
-                contentItems.eq(index).addClass('current').siblings().removeClass('current');
-            } else if (effect === 'fade') {
-                contentItems.eq(index).fadeIn().siblings().fadeOut();
-            } else {
-                contentItems.eq(index).addClass('current').siblings().removeClass('current');
+            
+            currentTab.addClass("active").siblings().removeClass('active');
+
+            //tab内容切换
+            var index=currentTab.index();
+            if(effect=='default'){
+                this.contentItems.eq(index).addClass('current').siblings().removeClass('current');
+            }else if(effect=='fade'){
+                this.contentItems.eq(index).fadeIn().siblings().fadeOut();
+            }else{
+                this.contentItems.eq(index).addClass('current').siblings().removeClass('current');
             }
+
             //如果配置的自动切换，要把当前的loop值设置成当前tab的index
             if (this.config.auto) {
                 this.loop = index;
             }
         },
-        autoPlay: function () {
-            var that = this,
-                tabItems = this.tabItems,//临时保存tab列表
-                tabLength = tabItems.size(),
-                config = this.config;
-            this.timer = window.setInterval(function () {
+
+        //自动间隔切换
+        autoPlay:function(){
+            var that=this;
+            that.timer=setInterval(function(){
                 that.loop++;
-                if (that.loop >= tabLength) {
-                    that.loop = 0;
+                if(that.loop==that.tabItems.size()){
+                    that.loop=0;
                 }
-                tabItems.eq(that.loop).trigger(config.triggerType);
-            }, config.auto);
+                that.tabItems.eq(that.loop).trigger(that.config.triggerType);
+            },that.config.auto);
         }
     };
 
+
+    
+    //多个调用
     Tab.init = function (tabs) {
         var that = this;
         tabs.each(function () {
@@ -113,6 +123,7 @@
             return this;    //jq链式调用
         }
     });
+
 
     window.Tab = Tab;
 
